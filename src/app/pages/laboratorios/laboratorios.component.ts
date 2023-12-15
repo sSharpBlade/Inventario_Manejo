2721b8c8e4b5c0c9a07070b82259e9de6a899b83
Hamilton Jumbo <hamil.jumbo@gmail.com>
2023-12-15 11:02:41 -0500

    Modificacion de los componentes

--- src/app/pages/laboratorios/laboratorios.component.ts
+++ src/app/pages/laboratorios/laboratorios.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-laboratorio',
   templateUrl: './laboratorios.component.html',
-  styleUrl: './/laboratorios.component.css'
+  styleUrl: './laboratorios.component.css'
 })
 export class LaboratoriosComponent {
 
