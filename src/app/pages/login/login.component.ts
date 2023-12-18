f30ffbae014fbd72b59c35ca3fd5f423e5ae1094
SharpBlade <yepezedwin760@gmail.com>
2023-12-17 22:12:40 -0500

    Modelo previsto

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent{
 
 }
