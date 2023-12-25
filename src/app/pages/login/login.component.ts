358a259f04324cf6b1672dffdcdf170c0612e812
SharpBlade <yepezedwin760@gmail.com>
2023-12-25 17:28:15 -0500

    Personalización de contenido
    Cambio de fondos e integración de animación

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent{
+
+export class LoginComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.cambio();
+  }
+
+  private cambio() {
+    if (typeof document !== 'undefined') {
+      const logregBox = document.querySelector('.login');
+      const loginLink = document.querySelector('.iniciar-enlace');
+      const registerLink = document.querySelector('.registrar-enlace');
+  
+      registerLink?.addEventListener('click', () => {
+        logregBox?.classList.add('active');
+      });
+  
+      loginLink?.addEventListener('click', () => {
+        logregBox?.classList.remove('active');
+      });
+    }
+  }
 
 }
