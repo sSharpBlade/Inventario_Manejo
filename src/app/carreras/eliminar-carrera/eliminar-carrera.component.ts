9575ca154386273bb9e740029afaa51a72334030
Karen-Mich <kguatumillo3914@uta.edu.ec>
2023-12-31 12:44:51 -0500

    Componente eliminar carreras modificacion

--- src/app/carreras/eliminar-carrera/eliminar-carrera.component.ts
+++ src/app/carreras/eliminar-carrera/eliminar-carrera.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CarrerasUniServiceService } from '../../services/carreras-uni.service.service';
+import { carrerasI } from '../../services/model.laboratorios';
 
 @Component({
   selector: 'app-eliminar-carrera',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './eliminar-carrera.component.css'
 })
 export class EliminarCarreraComponent {
+  public carrera: carrerasI[] = [];
+
+  constructor(private carrerasUService: CarrerasUniServiceService) {}
+  
+    ngOnInit(): void {
+      this.carrerasUService.obtenerTodasLasCarreras().subscribe(
+        data => {
+          this.carrera = data;
+        },
+      );
+    }
+
+    eliminar(value: any) {
+      this.carrerasUService.eliminarCarrera(value.value).subscribe(
+        res => {
+          this.carrerasUService.obtenerTodasLasCarreras().subscribe(
+            data => {
+              this.carrera = data;
+            },
+          );
+        }
+      );
+    }
 
 }
