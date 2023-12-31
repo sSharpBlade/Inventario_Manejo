fb2d6a150415d1cc4b7d3c420ad00cebe6e81eea
Karen-Mich <kguatumillo3914@uta.edu.ec>
2023-12-31 01:04:59 -0500

    Realizo componente crear carrera

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,19 @@ import { AddLabComponent } from './laboratorios/add-lab/add-lab.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContPrincipalComponent } from './user/principal/cont-principal/cont-principal.component';
 
+import { CarrerasComponent } from './carreras/carreras.component';
+import { CrearCarrerasComponent } from './carreras/crear-carreras/crear-carreras.component';
+
 
 const routes: Routes = [
   {path: 'nav-lab', component: LaboratorioComponent},
   {path: 'delete-lab', component: DeleteLabComponent},
   {path: 'add-lab', component: AddLabComponent},
   {path: 'sidebar-user', component: SidebarComponent},
-  {path: 'principal', component: ContPrincipalComponent }
+  {path: 'principal', component: ContPrincipalComponent },
+
+  {path: 'nav-car', component: CarrerasComponent},
+  {path: 'crear-car', component: CrearCarrerasComponent}
 ];
 
 @NgModule({
