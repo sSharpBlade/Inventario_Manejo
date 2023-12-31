ba21633be23471b89e6208caf088b8720fd95bc6
SharpBlade <yepezedwin760@gmail.com>
2023-12-30 20:24:02 -0500

    Bugs import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { CommentsComponent } from './pages/comments/comments.component';
 import { CommentsAdminComponent } from './pages/comments-admin/comments-admin.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
-import { provideHttpClient } from '@angular/common/http';
 import { UsuComponent } from './pages/usu/usu.component';
 import { LabComponent } from './pages/lab/lab.component';
 import { AdminComponent } from './pages/admin/admin.component';
@@ -33,7 +32,6 @@ import { AdminComponent } from './pages/admin/admin.component';
   providers: [
     provideHttpClient()
   ],
-  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
