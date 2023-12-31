593e6538bb41e425eb73ea8970462daf1f8dbbe5
Karen-Mich <kguatumillo3914@uta.edu.ec>
2023-12-31 12:11:48 -0500

    Estilos componente carrera

--- src/app/services/carreras-uni.service.service.ts
+++ src/app/services/carreras-uni.service.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { carrerasI } from './model.laboratorios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarrerasUniServiceService {
 
-  constructor() { }
+  API: string = 'http://localhost/app_manejo/'
+
+  constructor(private http:HttpClient) { }
+
+  obtenerTodasLasCarreras(): Observable<carrerasI[]> {
+    return this.http.post<carrerasI[]>(this.API + "?todasLasCarreras",'');
+  }
+
+  insertCar(id_car:string, nom_car:string): Observable<any> {
+    const datos = {id_car:id_car, nom_car:nom_car }
+    return this.http.post(this.API + "?insertarCarrera", datos);
+  }
+
+  obtenerCarrera(): Observable<carrerasI[]> {
+    return this.http.post<carrerasI[]>(this.API + "?carrerasCreadas",'');
+  }
+
+  eliminarCarrera(id_car: string): Observable<any> {
+    const valor = { id_car: id_car };
+    return this.http.post(this.API + "?eliminarCarrera", valor);
+  }
+
+
 }
