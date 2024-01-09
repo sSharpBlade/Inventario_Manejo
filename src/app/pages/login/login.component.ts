032720c1e0bd13836c0a93e076fe883f170fa136
Hamilton Jumbo <hamil.jumbo@gmail.com>
2024-01-09 09:09:05 -0500

    Realizacion de todos los metodos para resolver el problema con laboratorios

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -48,7 +48,7 @@ export class LoginComponent implements OnInit {
           } else if (rol === 'usuario') {
             this.router.navigate(['/sidebar']);
           } else if (rol === 'admin') {
-            this.router.navigate(['/sidebar-user']);
+            this.router.navigate(['/sidebar-user/delete-lab']);
           } else {
             this.router.navigate(['']);
           }
